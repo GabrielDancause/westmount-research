3e087913a3085c1c668cb7e519facc6ec249a2e8
GabrielDancause <1530748+GabrielDancause@users.noreply.github.com>
2026-03-09 20:04:11 +0000

    Remove helper scripts from Share Dilution PR

--- test_yf17.cjs
+++
@@ -1,8 +0,0 @@
-const YahooFinance = require('yahoo-finance2');
-const yahooFinance = new YahooFinance.default({ suppressNotices: ['yahooSurvey'] });
-
-async function test() {
-  const tsData = await yahooFinance._env.fetch('https://query2.finance.yahoo.com/ws/fundamentals-timeseries/v1/finance/timeseries/AAPL?period1=1577836800&period2=1767225600&type=annualBasicAverageShares&merge=false');
-  console.log(await tsData.json());
-}
-test();
