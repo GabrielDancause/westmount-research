f85425bdc5ba0744b190501c8d62d0bb44843302
GabrielDancause <1530748+GabrielDancause@users.noreply.github.com>
2026-03-09 20:05:24 +0000

    feat: clean PR for profit margins study

--- test-yf.cjs
+++
@@ -1,8 +0,0 @@
-const YahooFinance = require('yahoo-finance2');
-const yahooFinance = new YahooFinance.default({ suppressNotices: ['yahooSurvey'] });
-async function test() {
-  const summary = await yahooFinance.quoteSummary('AAPL', { modules: ['summaryProfile', 'financialData'] });
-  console.log(summary.summaryProfile.sector, summary.summaryProfile.industry);
-  console.log(summary.financialData.profitMargins, summary.financialData.operatingMargins);
-}
-test();
